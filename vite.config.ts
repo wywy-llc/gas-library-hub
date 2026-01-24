6246534548379ff1a0b9318fdb9bfd26496f85c4
Fujisawa Yuki <g031y148yuki@gmail.com>
2026-01-24 16:16:14 +0900

    Mod: vitestの設定を更新し、storybookとの統合を追加。不要なファイルを削除。

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig(({ mode }) => {
             setupFiles: ['./vitest-setup-server.ts'],
           },
         },
+        './vitest.config.storybook.ts',
       ],
     },
   };
