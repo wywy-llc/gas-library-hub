d4633875f7ee51099f9fbc5d018ea59b01f4b0fe
Fujisawa Yuki <g031y148yuki@gmail.com>
2026-01-24 20:42:50 +0900

    feat: Add usage_example column to library_summary and update journal with new migration

--- drizzle/0005_add_usage_example_jsonb.sql
+++
@@ -1,2 +0,0 @@
--- Add structured usage_example column (jsonb) for UsageExampleAnnotated format
-ALTER TABLE "library_summary" ADD COLUMN "usage_example" jsonb;
---
+++ drizzle/0005_sticky_proudstar.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "library_summary" ADD COLUMN "usage_example" jsonb;
