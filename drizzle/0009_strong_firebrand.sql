f8348df9365de29a5bc5c4f1c34b7703a10b16a7
Fujisawa Yuki <g031y148yuki@gmail.com>
2026-01-25 16:50:33 +0900

    feat: user_notificationテーブルのis_readカラムのデータ型をbooleanに変更

--- drizzle/0009_strong_firebrand.sql
+++ drizzle/0009_strong_firebrand.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "user_notification" ALTER COLUMN "is_read" SET DATA TYPE boolean USING (is_read::int::boolean);
+ALTER TABLE "user_notification" ALTER COLUMN "is_read" SET DATA TYPE boolean USING (is_read != 0);
